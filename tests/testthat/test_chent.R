32caaedcc6b6b8addc9f23fce9dca4188f692fed
Johannes Ranke <johannes.ranke@jrwb.de>
2026-03-19 09:36:26 +0100

    Adapt test
    Also, only check availability of PubChem and BCPC once, this should
    really be sufficient.

--- tests/testthat/test_chent.R
+++ tests/testthat/test_chent.R
@@ -4,10 +4,9 @@ test_that("We can initialise an object and add information", {
   expect_equal(oct$inchikey, structure(NA, source = "user"))
   expect_equal(oct$smiles, c(user = "CCCCCCCCO"))
 
-  if (webchem::ping_service("pc")) {
+  if (pc_up) {
     oct$try_pubchem() |> 
-      expect_message("Querying PubChem for name 1-octanol") |> 
-      expect_warning("Overwriting uninitialized InChIKey")
+      expect_message("Querying PubChem for name 1-octanol")
     expect_equal(oct$inchikey, structure("KBPLFHHGFOOTCA-UHFFFAOYSA-N", source = "pubchem"))
     expect_equal(oct$smiles[["PubChem"]], "CCCCCCCCO")
     expect_equal(round(oct$mw, 2), structure(130.23, source = "pubchem"))
@@ -16,10 +15,6 @@ test_that("We can initialise an object and add information", {
   skip_if_no_rdkit()
   oct$get_rdkit() |> 
     expect_message("Get chemical information from RDKit using user SMILES")
-
-  # Check availability of BCPC and PubChem immediately before use
-  bcpc_up <- webchem::ping_service("bcpc")
-  pc_up <- webchem::ping_service("pc")
 
   skip_if_not(pc_up)
   expect_snapshot(print(oct))
